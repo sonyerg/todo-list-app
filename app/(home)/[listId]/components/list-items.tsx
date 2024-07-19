22d401b454f682550542f32a98182210ad07b9ce
sonyerg <ericksonrguinto@gmail.com>
2024-07-19 22:05:01 +1200

    added conditional line-through on list items

--- app/(home)/[listId]/components/list-items.tsx
+++ app/(home)/[listId]/components/list-items.tsx
@@ -4,18 +4,35 @@ import { Item } from "@prisma/client";
 
 import ListItemForm from "./list-item-form";
 import { Checkbox } from "@/components/ui/checkbox";
+import { useState } from "react";
 
 //TODO: add edit and delete function for items.
 //TODO: add initial data in list-item-form and make if-else whether to patch or to post.
 
 export default function ListItems({ listItems }: { listItems: Item[] }) {
+  const [checkedItems, setCheckedItems] = useState<boolean[]>(
+    Array(listItems.length).fill(false)
+  );
+
+  function onCheck(index: number) {
+    const newCheckedItems = [...checkedItems];
+    newCheckedItems[index] = !newCheckedItems[index];
+    // Toggle checked state for the specific item
+
+    setCheckedItems(newCheckedItems);
+  }
+
   return (
     <div className="mt-2">
-      {listItems.map((list) => (
+      {listItems.map((list, index) => (
         <div key={list.id} className="flex flex-row items-center gap-2 py-2">
-          <Checkbox />
+          <Checkbox onCheckedChange={() => onCheck(index)} />
           <div className="md:w-[300px] w-[350px]">
-            <p className="text-base overflow-hidden overflow-ellipsis">{`${list.item}`}</p>
+            <p
+              className={`text-base overflow-hidden overflow-ellipsis ${
+                checkedItems[index] ? "line-through" : ""
+              }`}
+            >{`${list.item}`}</p>
           </div>
         </div>
       ))}
