c7f1503c5d9e36f06907bca1dc892f53d2f6990a
sonyerg <ericksonrguinto@gmail.com>
2024-07-21 10:00:12 +1200

    fixed list not deleting when list not selected

--- components/lists.tsx
+++ components/lists.tsx
@@ -9,25 +9,26 @@ import toast from "react-hot-toast";
 
 import { Button } from "@/components/ui/button";
 import AlertModal from "@/components/modals/alert-modal";
-import { useParams, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function Lists({ lists }: { lists: List[] }) {
-  const params = useParams();
   const router = useRouter();
-
+  const [selectedListId, setSelectedListId] = useState<string | null>(null);
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  async function onDelete() {
+  async function onDelete(listId: string) {
     try {
       setLoading(true);
 
-      await axios.delete(`/api/lists/${params.listId}`);
-      window.location.assign("/");
+      await axios.delete(`/api/lists/${listId}`);
       router.refresh();
-      toast.success("Store deleted");
+      toast.success("List deleted");
     } catch (error: any) {
       toast.error("Error deleting list.");
+    } finally {
+      setOpen(false);
+      setLoading(false);
     }
   }
 
@@ -37,7 +38,11 @@ export default function Lists({ lists }: { lists: List[] }) {
         isOpen={open}
         onClose={() => setOpen(false)}
         loading={loading}
-        onConfirm={onDelete}
+        onConfirm={() => {
+          if (selectedListId) {
+            onDelete(selectedListId);
+          }
+        }}
       />
       <div className="flex flex-col gap-1 border rounded-md overflow-auto py-2 pr-2">
         {lists.length === 0 && (
@@ -60,7 +65,10 @@ export default function Lists({ lists }: { lists: List[] }) {
               variant="destructive"
               size="sm"
               className="h-8 w-8 opacity-0 group-hover:opacity-100 p-0"
-              onClick={() => setOpen(true)}
+              onClick={() => {
+                setSelectedListId(list.id);
+                setOpen(true);
+              }}
             >
               <Trash className="h-4 w-4" />
             </Button>
